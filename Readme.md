7ae211ee55751f69912a107e6385642415fcea63
Domdae <domdae.ph@gmail.com>
2023-03-22 18:17:45 +0700

    [tight-coupling]: many game -> game runner class is tightly coupled to a specific.

---
+++ Readme.md
@@ -0,0 +1,5 @@
+**Why is Coupling important?**
+measure of how much work is involved in changing something.
+
+**_We want to loose-coupling as much as possible._**
+We want to be able to make functional changes with as less code changes as possible.
--- src/main/java/com/in28minutes/learnspringframework/AppGamingBasicJava.java
+++ src/main/java/com/in28minutes/learnspringframework/AppGamingBasicJava.java
@@ -2,11 +2,13 @@ package com.in28minutes.learnspringframework;
 
 import com.in28minutes.learnspringframework.Game.GameRunner;
 import com.in28minutes.learnspringframework.Game.MarioGame;
+import com.in28minutes.learnspringframework.Game.SuperContraGame;
 
 public class AppGamingBasicJava {
     public static void main(String[] args) {
-        var marioGame = new MarioGame();
-        var gameRunner = new GameRunner(marioGame);
+//        var marioGame = new MarioGame();
+        var superContraGame = new SuperContraGame();
+        var gameRunner = new GameRunner(superContraGame);
         gameRunner.run();
     }
 }
