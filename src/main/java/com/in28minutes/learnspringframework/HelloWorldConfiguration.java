441a590de862ba2829cc5215e6b900328193c160
Domdae <domdae.ph@gmail.com>
2023-03-27 16:31:25 +0700

    [Spring Context]: 2

--- src/main/java/com/in28minutes/learnspringframework/HelloWorldConfiguration.java
+++ src/main/java/com/in28minutes/learnspringframework/HelloWorldConfiguration.java
@@ -3,7 +3,7 @@ package com.in28minutes.learnspringframework;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-record Person (String name, int age) { };
+record Person (String name, int age, Address address) { };
 
 record Address (String firstLine, String city) { };
 
@@ -22,11 +22,26 @@ public class HelloWorldConfiguration {
 
     @Bean
     public Person person() {
-        return new Person("F2", 23);
+        return new Person("F2", 23, new Address("yo", "to"));
     }
 
     @Bean
-    public Address address() {
+    public Person person2MethodCall() {
+        return new Person(name(), age(), yo()); // name, age
+    }
+
+    @Bean
+    public Person person3Parameters(String name, int age, Address address3) {
+        return new Person(name, age, address3); // name, age
+    }
+
+    @Bean (name = "address")
+    public Address yo() {
         return new Address("Chang", "Chiang Mai");
+    }
+
+    @Bean (name = "address3")
+    public Address address() {
+        return new Address("Chang", "Chiang Mai2");
     }
 }
